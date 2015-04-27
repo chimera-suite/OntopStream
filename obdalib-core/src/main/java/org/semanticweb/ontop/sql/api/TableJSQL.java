adb4dc656a61689615971167a6aa4fa8f12f91fc
Benjamin Cogrel <benjamin.cogrel@bcgl.fr>
2015-04-27 14:03:26 +0200

    Unquote parsed aliases.

--- obdalib-core/src/main/java/org/semanticweb/ontop/sql/api/TableJSQL.java
+++ obdalib-core/src/main/java/org/semanticweb/ontop/sql/api/TableJSQL.java
@@ -144,22 +144,12 @@ public class TableJSQL implements Serializable{
 		if (alias == null) {
 			return;
 		}
+		alias.setName(unquote(alias.getName()));
 		this.alias = alias;
-		this.aliasName = alias.getName();
-	}
-
-	public void setAliasName(String alias) {
-		this.aliasName = alias;
-		if (alias != null)
-			this.alias.setName(alias);
 	}
 	
 	public Alias getAlias() {
 		return alias;
-	}
-	
-	public String getAliasName() { 
-		return this.aliasName;
 	}
 	
 	/**
@@ -199,5 +189,13 @@ public class TableJSQL implements Serializable{
 		return false;
 	}
 
-	
+	/**
+	 * Idempotent method.
+	 */
+	public static String unquote(String name) {
+		if(ParsedSQLQuery.pQuotes.matcher(name).matches()) {
+			return name.substring(1, name.length()-1);
+		}
+		return name;
+	}
 }
